eecb31d2dfd81a6ad88685953209f6920b443d0e
Quimey Arozarena <quimeyarozarena@gmail.com>
2016-02-22 12:47:41 -0300

    commit

--- Blog/src/main/java/com/blog/model/Entry.java
+++ Blog/src/main/java/com/blog/model/Entry.java
@@ -1,38 +1,106 @@
 package com.blog.model;
 
 import java.util.Date;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "entries")
 public class Entry {
 
-    private int id;
-    private String username;
+    @Column(name = "entryid", nullable = false)
+    @Id
+    @GeneratedValue
+    private int entryid;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = { CascadeType.ALL })
+    @JoinColumn(name = "userid", nullable = false) // FK
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = { CascadeType.ALL })
+    @JoinColumn(name = "postid", nullable = false) // FK
+    private Post post;
+
+    @Column(name = "description", nullable = false)
     private String description;
+
+    @Column(name = "date_entry", nullable = false)
     private Date date;
 
     public Entry() {
     }
 
-    public Entry(int id, String username, String description, Date date) {
-        this.id = id;
-        this.username = username;
+    public Entry(User user, String description) {
+        this.user = user;
+        this.description = description;
+    }
+
+    public Entry(User user, String description, Date date) {
+        this.user = user;
         this.description = description;
         this.date = date;
     }
 
+    public Entry(int entryid, User user, String description, Date date) {
+        this.entryid = entryid;
+        this.user = user;
+        this.description = description;
+        this.date = date;
+    }
+
+    public Entry(User user, Post post, String description, Date date) {
+        super();
+        this.user = user;
+        this.post = post;
+        this.description = description;
+        this.date = date;
+    }
+
+    public int getEntryid() {
+        return entryid;
+    }
+
+    public void setEntryid(int entryid) {
+        this.entryid = entryid;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
     public int getId() {
-        return id;
+        return entryid;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setId(int entryid) {
+        this.entryid = entryid;
     }
 
-    public String getUsername() {
-        return username;
+    public User getUsername() {
+        return user;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setUsername(User user) {
+        this.user = user;
     }
 
     public String getDescription() {
